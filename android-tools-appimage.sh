fca62eef661c54413c4cc23e73fee5db7de56324
Samueru-sama <36420837+Samueru-sama@users.noreply.github.com>
2024-04-11 22:26:44 -0400

    Add udev rules installer

--- android-tools-appimage.sh
+++ android-tools-appimage.sh
@@ -33,7 +33,7 @@ ln -s ./Android.png ./.DirIcon
 cat >> ./AppRun << 'EOF'
 #!/bin/bash
 CURRENTDIR="$(readlink -f "$(dirname "$0")")"
-UDEVNOTICE=$(echo "If you get errors it might be because you don't have the android udev rules for your device" )
+UDEVNOTICE=$(echo "If you get errors it might be because of missing android udev rules, use --getudev to install them")
 if [ "$1" = "adb" ]; then
 	"$CURRENTDIR"/adb "${@:2}" || echo "$UDEVNOTICE"
 	elif [ "$1" = "etc1tool" ]; then
@@ -48,6 +48,19 @@ if [ "$1" = "adb" ]; then
 	"$CURRENTDIR"/mke2fs "${@:2}" || echo "$UDEVNOTICE"
 	elif [ "$1" = "sqlite3" ]; then
 	"$CURRENTDIR"/sqlite3 "${@:2}" || echo "$UDEVNOTICE"
+	elif [ "$1" = "--getudev" ]; then
+	if cat /etc/udev/rules.d/*droid.rules > /dev/null; then
+		echo "udev rules already installed"
+		echo “Errors persisting with installed udev rules may be due to specific phone missing from the rules or insufficient permissions on the phone”
+	else
+		UDEVREPO=https://github.com/M0Rf30/android-udev-rules.git	
+		git clone $UDEVREPO
+		cd android-udev-rules
+		chmod a+x ./install.sh
+		echo "udev rules installer from $UDEVREPO"
+		sudo ./install.sh
+		cd .. && cat /etc/udev/rules.d/*droid.rules > /dev/null && rm -rf "./android-udev-rules"
+	fi
 else
 	echo "Error: No command specified, try \"./*tools.AppImage adb shell\" for example"
 	echo "You can also use aliases or wrapper scripts to not write ./*tools.AppImage every time"
